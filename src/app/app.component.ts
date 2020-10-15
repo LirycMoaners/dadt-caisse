0f06426ad4c20e8376e6211ed4600d93105c1192
Cyril Lefebvre <cyril.lefebvre.27@gmail.com>
2020-10-15 23:32:25 +0200

    feat(AppModule) Add localize & french

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+  constructor(
+    private readonly swUpdate: SwUpdate,
+  ) { }
+
+  ngOnInit(): void {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe(() => {
+        if (confirm('Une nouvelle version est disponible ! Souhaitez-vous recharger votre page pour la visualiser ?')) {
+          window.location.reload();
+        }
+      });
+    }
+  }
+}
