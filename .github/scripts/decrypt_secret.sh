fa004170a0f242f5955e811d510d43eccb05bf8d
Cyril Lefebvre <cyril.lefebvre.27@gmail.com>
2020-12-31 11:02:26 +0100

    chore(decrypt_secret) Change file path

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -3,7 +3,7 @@
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output $HOME/src/assets/secrets/google_client.json secrets/google_client.gpg
+--output ./src/assets/secrets/google-client.json ./.github/secrets/google-client.json.gpg
 
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output $HOME/src/assets/secrets/google_client.prod.json secrets/google_client.prod.gpg
+--output ./src/assets/secrets/google-client.prod.json ./.github/secrets/google-client.prod.json.gpg
