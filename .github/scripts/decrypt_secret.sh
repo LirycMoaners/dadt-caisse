637f400052989b83dc336e7c53fa9f4b58dd9368
Cyril Lefebvre <cyril.lefebvre.27@gmail.com>
2020-12-31 10:38:38 +0100

    chore(decrypt_secret) Add $HOME

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -3,7 +3,7 @@
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output src/assets/secrets/google_client.json src/assets/secure/google_client.gpg
+--output $HOME/src/assets/secrets/google_client.json src/assets/secure/google_client.gpg
 
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output src/assets/secrets/google_client.prod.json src/assets/secure/google_client.prod.gpg
+--output $HOME/src/assets/secrets/google_client.prod.json src/assets/secure/google_client.prod.gpg
