c67e52f5907ceab331d014d58ce3ee127f466a9d
Cyril Lefebvre <cyril.lefebvre.27@gmail.com>
2020-12-31 11:05:57 +0100

    chore(decrypt_secret) Create secret folder

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# Decrypt the file
+mkdir ./src/assets/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
